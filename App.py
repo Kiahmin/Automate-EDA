ac322a20b0c5c9d1bef2876924e4c5aed12501ca
Kiahmin <kiahminh@gmail.com>
2024-08-15 11:29:03 +0800

    Added tab2 and tab3 and requirements file

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -33,10 +34,10 @@ def create_missing_values_chart(df):
 def find_cont_cat_cols(df):
     cont, cat = [], []
     for col in df.columns:
-        if pd.api.types.is_numeric_dtype(df[col]) and len(df[col].unique()) > 25:
-            cont.append(col.strip())
-        else:
+        if len(df[col].unique()) <= 25 or df[col].dtype == np.object_:
             cat.append(col.strip())
+        else:
+            cont.append(col.strip())
     return cont, cat
 
 ## Web App
@@ -77,11 +78,62 @@ if uploaded_file is not None:
             st.pyplot(corr_fig, use_container_width=True)
 
             st.subheader("4. Missing Values Distribution")
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {}".format("Missing Values", df.isna().sum()), unsafe_allow_html=True)
             missing_fig = create_missing_values_chart(df)
             st.pyplot(missing_fig, use_container_width=True)
         
         with tab2:
-            pass
+            # Describe 
+            describe_df = df.describe()
+            st.subheader("Analyze Individual Feature Distributions")
+
+            # Continous Features
+            st.markdown("### 1. Understand Continuous Features")
+            cont_feature = st.selectbox(label = "Select Continous Feature", options = cont, index = 0)
+
+            cnt_na = df[cont_feature].isna().sum()
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {}".format("Count", describe_df[cont_feature]['count']), unsafe_allow_html=True)
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {} / ({:.2f} %)".format("Missing Values Count", cnt_na, cnt_na/df.shape[0]), unsafe_allow_html=True)
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {:.2f}".format("Mean", describe_df[cont_feature]['mean']), unsafe_allow_html=True)
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {:.2f}".format("Standard Deviation", describe_df[cont_feature]['std']), unsafe_allow_html=True)
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {}".format("Minimum", describe_df[cont_feature]['min']), unsafe_allow_html=True)
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {}".format("Maximum", describe_df[cont_feature]['max']), unsafe_allow_html=True)
+            st.markdown("<span style='font-weight:bold;'>{}</span> :".format("Quantiles"), unsafe_allow_html=True)
+            st.write(describe_df[[cont_feature]].T[['25%', "50%", "75%"]])
+
+            # Distribution of cont features
+            hist = df.plot_bokeh.hist(y=cont_feature, bins=50, legend=False, vertical_xlabel=True, show_figure=False)
+            st.bokeh_chart(hist, use_container_width=True)
+
+            # Categorical Features
+            st.subheader("#### 2. Understand Categorical Features")
+            cat_feature = st.selectbox(label="Select Categorical Feature", options=cat, index=0)
+
+            cnt_na = df[cont_feature].isna().sum()
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {}".format("Count", describe_df[cont_feature]['count']), unsafe_allow_html=True)
+            st.markdown("<span style='font-weight:bold;'>{}</span> : {} / ({:.2f} %)".format("Missing Values Count", cnt_na, cnt_na/df.shape[0]), unsafe_allow_html=True)
+
+            # Distribution of cat columns
+            df[cat_feature] = df[cat_feature].fillna("Missing")
+            cnt_feature = Counter(df[cat_feature].values)
+            df_cnt = pd.DataFrame({"Value": cnt_feature.keys(), "Count": cnt_feature.values()})
+            bar_fig = df_cnt.plot_bokeh.bar(x="Value", y="Count", color="tomato", legend=False, show_figure=False)
+            st.bokeh_chart(bar_fig, use_container_width=True)
 
         with tab3:
-            pass
+            st.subheader("Explore Relationships between Features")
+            colA, colB = st.columns(2)
+            with colA:
+                x = st.selectbox(label="X_axis", options=cont, index=0)
+            with colB:
+                y = st.selectbox(label="Y_axis", options=cont, index=0)
+
+            color_by = st.selectbox(label="Color by", options=[None,] + cat )
+
+            scatter_plt = df.plot_bokeh.scatter(x=x, 
+                                                y=y,
+                                                category=color_by if color_by else None,
+                                                title="{} vs {}".format(x.capitalize(), y.capitalize()),
+                                                figsize=(600,500),
+                                                show_figure=False)
+            st.bokeh_chart(scatter_plt, use_container_width=True)
